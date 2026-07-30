89426dbd853056f7d131e0a482e358035885d68c
Gauri Vinod Kirad <gauri@Gauris-MacBook-Air.local>
2026-07-30 17:54:32 +0530

    feat: implement JWT token generation

---
+++ src/main/java/com/hireflow/backend/service/JwtService.java
@@ -0,0 +1,46 @@
+package com.hireflow.backend.service;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+import javax.crypto.SecretKey;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.hireflow.backend.entity.User;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+
+@Service
+public class JwtService {
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    @Value("${jwt.expiration}")
+    private long jwtExpiration;
+
+    public String generateToken(User user) {
+
+        return Jwts.builder()
+                .subject(user.getEmail())
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + jwtExpiration))
+                .signWith(getSigningKey())
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+        return "";
+    }
+
+    public boolean isTokenValid(String token, User user) {
+        return false;
+    }
+
+    private SecretKey getSigningKey() {
+        return Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
+    }
+}
